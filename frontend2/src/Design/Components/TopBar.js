a0761b2021dc5ba85ebf231ee605719da1e56c0f
Darshana Perera <ds.perera.1997@gmail.com>
2025-01-18 10:22:19 +0530

    logout button added

--- frontend2/src/Design/Components/TopBar.js
+++ frontend2/src/Design/Components/TopBar.js
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 import logo from "../Assets/logoWhite.png";
 
 const TopBar = () => {
+  const handleLogout = () => {
+    // Refresh the page when the logout button is clicked
+    window.location.reload();
+  };
+
   return (
-    <div className="top-bar ">
-      <Link to="/dashboard" className="text-decoration-none my-4">
+    <div className="top-bar d-flex justify-content-between align-items-center p-3">
+      <Link to="/dashboard" className="text-decoration-none">
         <img
           src={logo} // Replace with the correct path to your PNG
           alt="Icon"
           className="top-bar-icon"
         />
-        {/* add dropdown here */}
       </Link>
+      <button className="btn btn-primary2" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 };
