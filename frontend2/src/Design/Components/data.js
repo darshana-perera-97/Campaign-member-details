50c8e2550a0e69ce28a1064ba13785240ce58350
Darshana Perera <ds.perera.1997@gmail.com>
2025-02-04 20:59:27 +0530

    new data added

--- frontend2/src/Design/Components/data.js
+++ frontend2/src/Design/Components/data.js
@@ -1,12 +1,435 @@
 export const poolingOptions = [
-  { value: "001 - l=vdjej", label: "001 - l=vdjej" },
-  { value: "002 - uyjej", label: "002 - uyjej" },
-  { value: "003 - fldiajdäh", label: "003 - fldiajdäh" },
+  {
+    value: "1' y,d$Ydka; wdkd lKsIaG úoHd,h - isß.ïm, -Yd,d wxl 01",
+    label: "1' y,d$Ydka; wdkd lKsIaG úoHd,h - isß.ïm, -Yd,d wxl 01",
+  },
+  {
+    value: "2' Wodisß.u m%cd Yd,dj",
+    label: "2' Wodisß.u m%cd Yd,dj",
+  },
+  {
+    value: "3' y,d$Ydka; wdkd lKsIaG úoHd,h - isß.ïm, -Yd,d wxl 02",
+    label: "3' y,d$Ydka; wdkd lKsIaG úoHd,h - isß.ïm, -Yd,d wxl 02",
+  },
+  {
+    value: "4' y,d$¨Kqú, fn!oaO m%d:ñl  úÿy, - Yd,d wxl 01",
+    label: "4' y,d$¨Kqú, fn!oaO m%d:ñl  úÿy, - Yd,d wxl 01",
+  },
+  {
+    value: "5' y,d$¨Kqú, fn!oaO m%d:ñl  úÿy, - Yd,d wxl 02",
+    label: "5' y,d$¨Kqú, fn!oaO m%d:ñl  úÿy, - Yd,d wxl 02",
+  },
+  {
+    value: "6' y,d$¨Kqú, fn!oaO m%d:ñl  úÿy, - Yd,d wxl 03",
+    label: "6' y,d$¨Kqú, fn!oaO m%d:ñl  úÿy, - Yd,d wxl 03",
+  },
+  {
+    value: "7' y,d$wkqreoaO uOH uyd úÿy, - ¨Kqú, - Yd,d wxl 01",
+    label: "7' y,d$wkqreoaO uOH uyd úÿy, - ¨Kqú, - Yd,d wxl 01",
+  },
+  {
+    value: "8' y,d$wkqreoaO uOH uyd úÿy, - ¨Kqú, - Yd,d wxl 02",
+    label: "8' y,d$wkqreoaO uOH uyd úÿy, - ¨Kqú, - Yd,d wxl 02",
+  },
+  {
+    value: "9' jhU $mq;a$y,d$fjka$nkaäßmamqj lksIaG úoHd,h - Yd,d wxl 01",
+    label: "9' jhU $mq;a$y,d$fjka$nkaäßmamqj lksIaG úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "10' jhU $mq;a$y,d$fjka$nkaäßmamqj lksIaG úoHd,h - Yd,d wxl 02",
+    label: "10' jhU $mq;a$y,d$fjka$nkaäßmamqj lksIaG úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "11' jhU $mq;a$y,d$fjka$nkaäßmamqj lksIaG úoHd,h - Yd,d wxl 03",
+    label: "11' jhU $mq;a$y,d$fjka$nkaäßmamqj lksIaG úoHd,h - Yd,d wxl 03",
+  },
+  {
+    value: "12' W;=re fld<skacdäh m%cd Yd,dj",
+    label: "12' W;=re fld<skacdäh m%cd Yd,dj",
+  },
+  {
+    value: "13' fjkakmamqj ifilaia úoHd,h - Yd,d wxl 01",
+    label: "13' fjkakmamqj ifilaia úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "14' fjkakmamqj ifilaia úoHd,h - Yd,d wxl 02",
+    label: "14' fjkakmamqj ifilaia úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "15' y,d$ idka; m%ekaisia fiaúh¾ úÿy, - W,aysáhdj - Yd,d wxl 01",
+    label: "15' y,d$ idka; m%ekaisia fiaúh¾ úÿy, - W,aysáhdj - Yd,d wxl 01",
+  },
+  {
+    value: "16' y,d$ idka; m%ekaisia fiaúh¾ úÿy, - W,aysáhdj - Yd,d wxl 02",
+    label: "16' y,d$ idka; m%ekaisia fiaúh¾ úÿy, - W,aysáhdj - Yd,d wxl 02",
+  },
+  {
+    value: "17' y,d$fjkakmamqj Ydka; fcdaYma jdia úoHd,h ^cqì,s Yd,dj&",
+    label: "17' y,d$fjkakmamqj Ydka; fcdaYma jdia úoHd,h ^cqì,s Yd,dj&",
+  },
+  {
+    value: "18' y,d$idka; m%ekaisia fiaúh¾ úÿy,  - W,aysáhdj  - Yd,d wxl 03",
+    label: "18' y,d$idka; m%ekaisia fiaúh¾ úÿy,  - W,aysáhdj  - Yd,d wxl 03",
+  },
+  {
+    value: "19' y,d$fjkakmamqj Ydka; fcdaYma jdia úoYd,h - m%d:ñl wxYh",
+    label: "19' y,d$fjkakmamqj Ydka; fcdaYma jdia úoYd,h - m%d:ñl wxYh",
+  },
+  {
+    value:
+      "20' y,d$fjka Ydka; udáka o fmdaria uyd úÿy, - fjkakmamqj - Yd,d wxl 01",
+    label:
+      "20' y,d$fjka Ydka; udáka o fmdaria uyd úÿy, - fjkakmamqj - Yd,d wxl 01",
+  },
+  {
+    value:
+      "21' y,d$fjka Ydka; udáka o fmdaria uyd úÿy, - fjkakmamqj - Yd,d wxl 02",
+    label:
+      "21' y,d$fjka Ydka; udáka o fmdaria uyd úÿy, - fjkakmamqj - Yd,d wxl 02",
+  },
+  {
+    value: "22' y,d$fjkakmamqj Y+oaO jQ mjqf,a nd,sld uyd úÿy, - Yd,d wxl 01",
+    label: "22' y,d$fjkakmamqj Y+oaO jQ mjqf,a nd,sld uyd úÿy, - Yd,d wxl 01",
+  },
+  {
+    value: "23' y,d$fjkakmamqj Y+oaO jQ mjqf,a nd,sld m%d:ñl úÿy, fjkakmamqj",
+    label: "23' y,d$fjkakmamqj Y+oaO jQ mjqf,a nd,sld m%d:ñl úÿy, fjkakmamqj",
+  },
+  {
+    value: "24' y,d$Ydka; Ígd uyd úoHd,h - ;,afol",
+    label: "24' y,d$Ydka; Ígd uyd úoHd,h - ;,afol",
+  },
+  {
+    value: "25' y,d$ÿïu,foKsh frdaudkq lf;da,sl úÿy,",
+    label: "25' y,d$ÿïu,foKsh frdaudkq lf;da,sl úÿy,",
+  },
+  {
+    value: "26' y,d$fjka$ÿïu,foKsh fn!oaO lksIaG úÿy, - ÿïu,foKsh",
+    label: "26' y,d$fjka$ÿïu,foKsh fn!oaO lksIaG úÿy, - ÿïu,foKsh",
+  },
+  {
+    value: "27' y,d$fjka$idka; ußhd m%d:ñl úÿy, - khskuvu Yd,d wxl 01",
+    label: "27' y,d$fjka$idka; ußhd m%d:ñl úÿy, - khskuvu Yd,d wxl 01",
+  },
+  {
+    value: "28' y,d$fjka$idka; ußhd m%d:ñl úÿy, - khskuvu Yd,d wxl 02",
+    label: "28' y,d$fjka$idka; ußhd m%d:ñl úÿy, - khskuvu Yd,d wxl 02",
+  },
+  {
+    value: "29' y,d$khskuvu uyd úoHd,h - Yd,d wxl 01",
+    label: "29' y,d$khskuvu uyd úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "30' y,d$khskuvu uyd úoHd,h - Yd,d wxl 02",
+    label: "30' y,d$khskuvu uyd úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "31' y,d$khskuvu uyd úoHd,h - Yd,d wxl 03",
+    label: "31' y,d$khskuvu uyd úoHd,h - Yd,d wxl 03",
+  },
+  {
+    value: "32' y,d$ñßiaikafldgqj ksu, ußhd m%d:ñl úoHd,h",
+    label: "32' y,d$ñßiaikafldgqj ksu, ußhd m%d:ñl úoHd,h",
+  },
+  {
+    value: "33' y,d$Ydka; wdkd uyd úoHd,h - fndr,eiai - Yd,d wxl 02",
+    label: "33' y,d$Ydka; wdkd uyd úoHd,h - fndr,eiai - Yd,d wxl 02",
+  },
+  {
+    value: "34' y,d$Ydka; wdkd uyd úoHd,h - fndr,eiai - Yd,d wxl 03",
+    label: "34' y,d$Ydka; wdkd uyd úoHd,h - fndr,eiai - Yd,d wxl 03",
+  },
+  {
+    value: "35' y,d$Ydka; wdkd uyd úoHd,h - fndr,eiai - Yd,d wxl 01",
+    label: "35' y,d$Ydka; wdkd uyd úoHd,h - fndr,eiai - Yd,d wxl 01",
+  },
+  {
+    value: "36' y,d$fjka Ydka; Tos,shd lksgq úÿy, - Yd,d wxl 01",
+    label: "36' y,d$fjka Ydka; Tos,shd lksgq úÿy, - Yd,d wxl 01",
+  },
+  {
+    value: "37' y,d$fjka Ydka; Tos,shd lksgq úÿy, - Yd,d wxl 02",
+    label: "37' y,d$fjka Ydka; Tos,shd lksgq úÿy, - Yd,d wxl 02",
+  },
+  {
+    value: "38' y,d$fjka Ydka; Tos,shd lksgq úÿy, - Yd,d wxl 03",
+    label: "38' y,d$fjka Ydka; Tos,shd lksgq úÿy, - Yd,d wxl 03",
+  },
+  {
+    value: "39' y,d$fjka$f.dkaid,afõia uyd úoHd,h - Yd,d wxl 01",
+    label: "39' y,d$fjka$f.dkaid,afõia uyd úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "40' y,d$fjka$f.dkaid,afõia uyd úoHd,h - Yd,d wxl 02",
+    label: "40' y,d$fjka$f.dkaid,afõia uyd úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "41' y,d$jhslald, lKsIaG úoHd,h - Yd,d wxl 01",
+    label: "41' y,d$jhslald, lKsIaG úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "42' y,d$jhslald, lKsIaG úoHd,h - Yd,d wxl 02",
+    label: "42' y,d$jhslald, lKsIaG úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "43' y,d$;Urú, lKsIaG úoHd,h",
+    label: "43' y,d$;Urú, lKsIaG úoHd,h",
+  },
+  {
+    value: "44' y,d$fjka ¨Kqú, frdaudkq lf;da,sl m%d:ñl úÿy,",
+    label: "44' y,d$fjka ¨Kqú, frdaudkq lf;da,sl m%d:ñl úÿy,",
+  },
+  {
+    value: "45' y,d$YS% O¾urdu nd,l úÿy, - Yd,d wxl 01",
+    label: "45' y,d$YS% O¾urdu nd,l úÿy, - Yd,d wxl 01",
+  },
+  {
+    value: "46' y,d$fjka lsßueáhdk fn!oaO nd,sld úÿy,",
+    label: "46' y,d$fjka lsßueáhdk fn!oaO nd,sld úÿy,",
+  },
+  {
+    value: "47' lsßueáhdk Ys% O¾udrdu nd,l úoHd,h - Yd,d wxl 02",
+    label: "47' lsßueáhdk Ys% O¾udrdu nd,l úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "48' y,d$YS% O¾urdu nd,l úÿy, - Yd,d wxl 03",
+    label: "48' y,d$YS% O¾urdu nd,l úÿy, - Yd,d wxl 03",
+  },
+  {
+    value: "49' y,d$cskr;k uyd úÿy, - Yd,d wxl 01",
+    label: "49' y,d$cskr;k uyd úÿy, - Yd,d wxl 01",
+  },
+  {
+    value: "50' fmd;=jgjk cskr;k uyd úÿy, - Yd,d wxl 02",
+    label: "50' fmd;=jgjk cskr;k uyd úÿy, - Yd,d wxl 02",
+  },
+  {
+    value: "51' YS% lafIa;%rdu úydria:dkh - lygú,",
+    label: "51' YS% lafIa;%rdu úydria:dkh - lygú,",
+  },
+  {
+    value: "52' uyhdhj;a; isxy fmr mdi,",
+    label: "52' uyhdhj;a; isxy fmr mdi,",
+  },
+  {
+    value: "53' ;=,dj, YS% kd.jkdrdu mqrdK úydrh O¾uYd,dj",
+    label: "53' ;=,dj, YS% kd.jkdrdu mqrdK úydrh O¾uYd,dj",
+  },
+  {
+    value: "54' j$y,d$fjka ue,a,j úÿy,",
+    label: "54' j$y,d$fjka ue,a,j úÿy,",
+  },
+  {
+    value: "55' YS% fidaudkkao <ore mdi,",
+    label: "55' YS% fidaudkkao <ore mdi,",
+  },
+  {
+    value: "56' y,d$fudfydÜáuq,a, m%d:ñl úÿy,",
+    label: "56' y,d$fudfydÜáuq,a, m%d:ñl úÿy,",
+  },
+  {
+    value: "57' y,d$fjka nqcacïfmd, lksIag úÿy,",
+    label: "57' y,d$fjka nqcacïfmd, lksIag úÿy,",
+  },
+  {
+    value: "58' YS% úY=oaOdrdu úydria:dkh",
+    label: "58' YS% úY=oaOdrdu úydria:dkh",
+  },
+  {
+    value: "59' y,d$fjka yd,ao`vqjk fn!oaO úoHd,h",
+    label: "59' y,d$fjka yd,ao`vqjk fn!oaO úoHd,h",
+  },
+  {
+    value: "60' y,d$fjka yd,ao`vqjk frdaudkq lf;da,sl úÿy, - Yd,d wxl 01",
+    label: "60' y,d$fjka yd,ao`vqjk frdaudkq lf;da,sl úÿy, - Yd,d wxl 01",
+  },
+  {
+    value: "61' y,d$fjka yd,ao`vqjk frdaudkq lf;da,sl úÿy, - Yd,d wxl 02",
+    label: "61' y,d$fjka yd,ao`vqjk frdaudkq lf;da,sl úÿy, - Yd,d wxl 02",
+  },
+  {
+    value: "62' y,d$fjka yd,ao`vqjk frdaudkq lf;da,sl úÿy, - Yd,d wxl 03",
+    label: "62' y,d$fjka yd,ao`vqjk frdaudkq lf;da,sl úÿy, - Yd,d wxl 03",
+  },
+  {
+    value: "63' ldrkdj;a; m%cdYd,dj",
+    label: "63' ldrkdj;a; m%cdYd,dj",
+  },
+  {
+    value: "64' y,d$fjka oxfldgqj nd,sld uyd úoHd,h ^ìhiaà%Ü Yd,dj&",
+    label: "64' y,d$fjka oxfldgqj nd,sld uyd úoHd,h ^ìhiaà%Ü Yd,dj&",
+  },
+  {
+    value: "65' y,d$fjka l<dY=Í u¾i,ska chfldä msh úÿy,",
+    label: "65' y,d$fjka l<dY=Í u¾i,ska chfldä msh úÿy,",
+  },
+  {
+    value: "66' y,d$fjka oxfldgqj nd,sld uyd úoHd,h",
+    label: "66' y,d$fjka oxfldgqj nd,sld uyd úoHd,h",
+  },
+  {
+    value: "67' O¾u m%oSmdrdu fn!oaO uOHia:dkh",
+    label: "67' O¾u m%oSmdrdu fn!oaO uOHia:dkh",
+  },
+  {
+    value: "68' y,d$fjka ÿïu,fldgqj lksIaG úoHd,h",
+    label: "68' y,d$fjka ÿïu,fldgqj lksIaG úoHd,h",
+  },
+  {
+    value: "69' YS% ix>fndaê uOH uyd úoHd,h ^cd;sl mdi,&",
+    label: "69' YS% ix>fndaê uOH uyd úoHd,h ^cd;sl mdi,&",
+  },
+  {
+    value: "70' ke$lgqflkao Ys% wNshisxydrdu úydria:dkh",
+    label: "70' ke$lgqflkao Ys% wNshisxydrdu úydria:dkh",
+  },
+  {
+    value: "71' ueo f.dakú, m%cdYd,dj",
+    label: "71' ueo f.dakú, m%cdYd,dj",
+  },
+  {
+    value: "72' y,d$fjka f.dakú, idka; fcdaima úoHd,h",
+    label: "72' y,d$fjka f.dakú, idka; fcdaima úoHd,h",
+  },
+  {
+    value: "73' y,d$fjka weáhdj, lKsIaG úoHd,h - Yd,d wxl 01",
+    label: "73' y,d$fjka weáhdj, lKsIaG úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "74' YS% wfYdaldrduh\" lerkaj;a;",
+    label: "74' YS% wfYdaldrduh\" lerkaj;a;",
+  },
+  {
+    value: "75' y,d$fjka weáhdj, lKsIaG úoHd,h - Yd,d wxl 02",
+    label: "75' y,d$fjka weáhdj, lKsIaG úoHd,h - Yd,d wxl 02",
+  },
+  {
+    value: "76' Yla;s fh!jk iudc m%cdYd,dj",
+    label: "76' Yla;s fh!jk iudc m%cdYd,dj",
+  },
+  {
+    value: "77' y,d oslafj, m%d:ñl úÿy,",
+    label: "77' y,d oslafj, m%d:ñl úÿy,",
+  },
+  {
+    value: "78' y,d$fjka jElv uyd úoHd,h - Yd,d wxl 01",
+    label: "78' y,d$fjka jElv uyd úoHd,h - Yd,d wxl 01",
+  },
+  {
+    value: "79' wegU fmr mdi,",
+    label: "79' wegU fmr mdi,",
+  },
+  {
+    value: "80' y,d$fjka jElv YS% r;kfcda;s m%d:ñl úÿy,",
+    label: "80' y,d$fjka jElv YS% r;kfcda;s m%d:ñl úÿy,",
+  },
 ];
 
 export const gsOptions = [
-  { value: "01 - by, l=vdjej", label: "01 - by, l=vdjej" },
-  { value: "02 - my< l=vdjej", label: "02 - my< l=vdjej" },
+  { value: "467 kef.kysr ¨Kqú,", label: "467 kef.kysr ¨Kqú," },
+  { value: "467 B W;=re isß.ïm,", label: "467 B W;=re isß.ïm," },
+  { value: "467 C Wodisß.u", label: "467 C Wodisß.u" },
+  { value: "467 A ol=Kq isß.ïm,", label: "467 A ol=Kq isß.ïm," },
+  { value: "467 D ueo ¨Kqú,", label: "467 D ueo ¨Kqú," },
+  { value: "467 E ol=Kq ¨Kqú,", label: "467 E ol=Kq ¨Kqú," },
+  { value: "467 F ngysr ¨Kqú,", label: "467 F ngysr ¨Kqú," },
+  { value: "476 W;=re nKaäßmamqj", label: "476 W;=re nKaäßmamqj" },
+  { value: "476 B ngysr nKaäßmamqj", label: "476 B ngysr nKaäßmamqj" },
+  { value: "476 A kef.kysr nKaäßmamqj", label: "476 A kef.kysr nKaäßmamqj" },
+  { value: "493 kef.kysr fld<skacdäh", label: "493 kef.kysr fld<skacdäh" },
+  { value: "493 B W;=re fld<skacdäh", label: "493 B W;=re fld<skacdäh" },
+  { value: "493 A ngysr fld<skacdäh", label: "493 A ngysr fld<skacdäh" },
+  { value: "493 C ol=Kq fld<skacdäh", label: "493 C ol=Kq fld<skacdäh" },
+  { value: "492 W;=re W,aysáhdj", label: "492 W;=re W,aysáhdj" },
+  { value: "492 C ngysr W,aysáhdj", label: "492 C ngysr W,aysáhdj" },
+  { value: "492 A ol=Kq W,aysáhdj", label: "492 A ol=Kq W,aysáhdj" },
+  { value: "492 D lvj;", label: "492 D lvj;" },
+  { value: "492 B ueo W,aysáhdj", label: "492 B ueo W,aysáhdj" },
+  { value: "491 W;=re fjkakmamqj", label: "491 W;=re fjkakmamqj" },
+  { value: "491 B ueo fjkakmamqj", label: "491 B ueo fjkakmamqj" },
+  { value: "491 D kef.kysr fjkakmamqj", label: "491 D kef.kysr fjkakmamqj" },
+  { value: "491 C ngysr fjkakmamqj", label: "491 C ngysr fjkakmamqj" },
+  { value: "491 A ol=Kq fjkakmamqj", label: "491 A ol=Kq fjkakmamqj" },
+  { value: "491 B ueo fjkakmamqj", label: "491 B ueo fjkakmamqj" },
+  { value: "489 ngysr ÿïu,foKsh", label: "489 ngysr ÿïu,foKsh" },
+  { value: "490 B ol=Kq ÿïu,foKsh", label: "490 B ol=Kq ÿïu,foKsh" },
+  { value: "489 A ueo ÿïu,foKsh", label: "489 A ueo ÿïu,foKsh" },
+  { value: "490 kef.kysr ÿïu,foKsh", label: "490 kef.kysr ÿïu,foKsh" },
+  { value: "490 A W;=re ÿïu,foKsh", label: "490 A W;=re ÿïu,foKsh" },
+  { value: "486 ngysr khskuvu", label: "486 ngysr khskuvu" },
+  { value: "486 B fj,a,uxlrh", label: "486 B fj,a,uxlrh" },
+  { value: "486 A ueo khskuvu", label: "486 A ueo khskuvu" },
+  { value: "485 kef.kysr khskuvu", label: "485 kef.kysr khskuvu" },
+  { value: "485 A yKf;dgqfmd,", label: "485 A yKf;dgqfmd," },
+  { value: "487 W;=re ñßiaikafldgqj", label: "487 W;=re ñßiaikafldgqj" },
+  { value: "487 A ol=KQ ñßiaikafldgqj", label: "487 A ol=KQ ñßiaikafldgqj" },
+  { value: "488 kef.kysr fndr,eiai", label: "488 kef.kysr fndr,eiai" },
+  { value: "488 B W;=re fndr,eiai", label: "488 B W;=re fndr,eiai" },
+  { value: "488 A ngysr fndr,eiai", label: "488 A ngysr fndr,eiai" },
+  { value: "488 C ol=Kq fndr,eiai", label: "488 C ol=Kq fndr,eiai" },
+  { value: "481 B ol=Kq lïu,", label: "481 B ol=Kq lïu," },
+  { value: "481 iskaød;S%h", label: "481 iskaød;S%h" },
+  { value: "481 A w¨;af;dg", label: "481 A w¨;af;dg" },
+  {
+    value: "483 rx.ïuq,a, yd W;=re lïu,",
+    label: "483 rx.ïuq,a, yd W;=re lïu,",
+  },
+  { value: "484 kef.kysr wx.ïmsáh", label: "484 kef.kysr wx.ïmsáh" },
+  { value: "484 B ngysr wx.ïmsáh", label: "484 B ngysr wx.ïmsáh" },
+  { value: "484 A fnda,j;a;", label: "484 A fnda,j;a;" },
+  { value: "482 W;=re jhslald,", label: "482 W;=re jhslald," },
+  { value: "482 A ol=Kq jhslald,", label: "482 A ol=Kq jhslald," },
+  { value: "482 B f;damamqf;dg", label: "482 B f;damamqf;dg" },
+  { value: "480 ;Urú,", label: "480 ;Urú," },
+  { value: "480 A fnda,dk", label: "480 A fnda,dk" },
+  { value: "466 D W;=re lsßueáhdk", label: "466 D W;=re lsßueáhdk" },
+  { value: "466 E ol=Kq lsßueáhdk", label: "466 E ol=Kq lsßueáhdk" },
+  { value: "466 F ngysr lsßueáhdk", label: "466 F ngysr lsßueáhdk" },
+  { value: "466 kef.kysr lsßueáhdk", label: "466 kef.kysr lsßueáhdk" },
+  { value: "466 C fnda;f,a.u", label: "466 C fnda;f,a.u" },
+  { value: "466 A ueo lsßueáhdk", label: "466 A ueo lsßueáhdk" },
+  { value: "466 B mxi,a yxosh", label: "466 B mxi,a yxosh" },
+  { value: "464 fmd;=jgjk", label: "464 fmd;=jgjk" },
+  { value: "464 A yqkaosrdfmd<", label: "464 A yqkaosrdfmd<" },
+  { value: "464 B ngysr lygú,", label: "464 B ngysr lygú," },
+  { value: "464 kef.kysr lygú,", label: "464 kef.kysr lygú," },
+  { value: "465 ;=,dj,", label: "465 ;=,dj," },
+  { value: "463 ue,a,j", label: "463 ue,a,j" },
+  { value: "462 ,sysßhd.u", label: "462 ,sysßhd.u" },
+  { value: "469 by, fudfydÜáuq,a,", label: "469 by, fudfydÜáuq,a," },
+  { value: "469 A my, fudfydÜáuq,a,", label: "469 A my, fudfydÜáuq,a," },
+  { value: "470 my, nqcacïfmd,", label: "470 my, nqcacïfmd," },
+  { value: "470 A by, nqcacïfmd,", label: "470 A by, nqcacïfmd," },
+  { value: "468 W;=re yd,ao`vqjk", label: "468 W;=re yd,ao`vqjk" },
+  { value: "468 C ngysr yd,ao`vqjk", label: "468 C ngysr yd,ao`vqjk" },
+  { value: "468 A ol=Kq yd,ao`vqjk", label: "468 A ol=Kq yd,ao`vqjk" },
+  { value: "468 B ueo yd,ao`vqjk", label: "468 B ueo yd,ao`vqjk" },
+  { value: "473 A fudrelal=,sh", label: "473 A fudrelal=,sh" },
+  { value: "473 f.dve,a,", label: "473 f.dve,a," },
+  { value: "474 A ol=Kq oxfldgqj", label: "474 A ol=Kq oxfldgqj" },
+  { value: "474 C ngysr oxfldgqj", label: "474 C ngysr oxfldgqj" },
+  { value: "474 W;=re oxfldgqj", label: "474 W;=re oxfldgqj" },
+  { value: "474 B kef.kysr oxfldgqj", label: "474 B kef.kysr oxfldgqj" },
+  { value: "474 D by< ÿïu,fldgqj", label: "474 D by< ÿïu,fldgqj" },
+  { value: "474 E my, ÿïu,fldgqj", label: "474 E my, ÿïu,fldgqj" },
+  { value: "475 kef.kysr lgqflkao", label: "475 kef.kysr lgqflkao" },
+  { value: "475 A ngysr lgqflkao", label: "475 A ngysr lgqflkao" },
+  { value: "475 B ueo lgqflkao", label: "475 B ueo lgqflkao" },
+  { value: "477 W;=re f.dakú,", label: "477 W;=re f.dakú," },
+  { value: "477 B ueo f.dkaú,", label: "477 B ueo f.dkaú," },
+  { value: "477 A ol=Kq f.dakú,", label: "477 A ol=Kq f.dakú," },
+  { value: "471 W;=re weáhdj,", label: "471 W;=re weáhdj," },
+  { value: "471 B fudgEuq,a,", label: "471 B fudgEuq,a," },
+  { value: "471 A ol=Kq weáhdj,", label: "471 A ol=Kq weáhdj," },
+  { value: "472 ueáfldgqj", label: "472 ueáfldgqj" },
+  { value: "472 A ;durl=,sh", label: "472 A ;durl=,sh" },
+  { value: "458 oslafj,", label: "458 oslafj," },
+  { value: "460 l`:jÉÑuq,a,", label: "460 l`:jÉÑuq,a," },
+  { value: "460 A isx.lal=,sh", label: "460 A isx.lal=,sh" },
+  { value: "461 fikaosßhduq,a,", label: "461 fikaosßhduq,a," },
+  { value: "459 fhda.shdk", label: "459 fhda.shdk" },
+  { value: "459 A cxl+rdfj,", label: "459 A cxl+rdfj," },
+  { value: "460 l`:jÉÑuq,a,", label: "460 l`:jÉÑuq,a," },
 ];
 
 export const agaOptions = [
